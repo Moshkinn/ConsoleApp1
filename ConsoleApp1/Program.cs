986b4f3c9cea64a44648e6692ee566b5ecf19553
moshkinn <natan.moshkin@brightmachines.com>
2020-05-02 17:06:50 +0300

    operator override

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,30 +8,65 @@ namespace SoloLearn
 {
     class Program
     {
-        class Clients
+        class Box
         {
-            private string[] names = new string[10];
-            public string this[int index]
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public Box(int h, int w)
             {
-                get
+                Height = h;
+                Width = w;
+            }
+            public static Box operator +(Box a, Box b)
+            {
+                int h = a.Height + b.Height;
+                int w = a.Width + b.Width;
+                Box res = new Box(h, w);
+                return res;
+            }
+            public static bool operator >(Box a, Box b)
+            {
+                if (a.Height > b.Height)
                 {
-                    return names[index];
+                    return true;
                 }
-                set
+                else
                 {
-                    names[index] = value;
+                    return false;
+                }
+            }
+            
+    
+            public static bool operator <(Box a, Box b)
+            {
+                if (a.Height < b.Height)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
         }
-        static void Main(string[] args)
+        static void func()
+            {
+            int xx = 1;
+            xx++;
+            Console.WriteLine("xx= " + xx);
+            }
+    static void Main(string[] args)
         {
-            Clients c = new Clients();
-            c[0] = "Dave";
-            c[1] = "Bob";
-            c[9] = "ggg";
-            c[8] = "hhh";
+            Box b1 = new Box(14, 3);
+            Box b2 = new Box(5, 7);
+            Box b3 = b1 + b2;
+            func();
 
-            Console.WriteLine(c[1]);
+            Console.WriteLine("b1 h = " + b1.Height);
+            Console.WriteLine("b2 h = " + b2.Height);
+
+            if (b1 > b2)
+                Console.WriteLine("b1 > b2");
         }
     }
 }
