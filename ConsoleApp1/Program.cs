156851b33af5aab75bee9fbfafbf79fbac7698aa
moshkinn <natan.moshkin@brightmachines.com>
2020-05-28 15:23:31 +0300

    dictionary

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,15 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> d = new Dictionary<string, int>();
-            d.Add("Uno", 1);
-            d.Add("One", 1);
-            d.Add("Dos", 2);
-            d.Add("Deux", 2);
-            d.Remove("One");  // Remove key-value pair One, 1
-            d.Remove("Dos");  // Remove key-value pair Dos, 2
+            // Dictionary<string, int> d = new Dictionary<string, int>();
 
+            Dictionary<string, string> d = new Dictionary<string, string>();
             Console.WriteLine("Dictionary: ");
+            d.Add("name", "Bob");
+            //d.Add("name", "Bobs");
+
+            d.Add("age", "25");
+            d.Add("sex", "male");
             foreach (string s in d.Keys)
                 Console.WriteLine(s + ": " + d[s]);  // Uno: 1  Deux: 2
             Console.WriteLine("\nCount: {0}", d.Count); // 2 
