0c95cb16f3ae88cafacac3ed12d1b754db9f17d6
moshkinn <natan.moshkin@brightmachines.com>
2020-05-28 11:20:22 +0300

    System.Collections.Generic List

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,52 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SoloLearn
 {
     class Program
     {
-        class Stack<T>
-        {
-            int index = 0;
-            T[] innerArray = new T[100];
-            public void Push(T item)
-            {
-                innerArray[index++] = item;
-            }
-            public T Pop()
-            {
-                return innerArray[--index];
-            }
-            public T Get(int k) { return innerArray[k]; }
-        }
         static void Main(string[] args)
         {
-            /*Stack<int> intStack = new Stack<int>();
-            intStack.Push(3);
-            Console.WriteLine(intStack.Pop());
-            intStack.Push(6);
-            Console.WriteLine(intStack.Pop());
-            intStack.Push(7);
-            Console.WriteLine(intStack.Pop());
-            //intStack.Push(8);
-            Console.WriteLine(intStack.Get(0));
-            */
-            ///////////////////////////////////////
-            Stack<string> sStack = new Stack<string>();
-            sStack.Push("a");
-            sStack.Pop();
-            sStack.Push("b");
-            sStack.Push("c");
-            Console.WriteLine(sStack.Get(0));
-            for(int i = 0; i<10; i++)
+            List<int> li = new List<int>();
+            li.Add(59);
+            li.Add(72);
+            li.Add(95);
+            li.Add(5);
+            li.Add(19);
+            li.Remove(59);
+            li.RemoveAt(1);
+            for (int i=0; i<li.Count; i++)
             {
-                sStack.Push("bb");
+                li[i] = i;
             }
-            Console.WriteLine(sStack.Get(10));
 
+            Console.WriteLine("Capacity: ");
+            Console.WriteLine(li.Capacity);
+            Console.Write("\nList: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " ");
+            li.Sort();
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " ");
+            //li.ForEach(+1);
         }
     }
 }
