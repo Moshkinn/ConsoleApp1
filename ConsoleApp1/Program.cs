08d55da085833b97992bdb145db55bfea41eea5e
moshkinn <natan.moshkin@brightmachines.com>
2020-05-28 13:28:23 +0300

    sorted list

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,29 +7,21 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            List<int> li = new List<int>();
-            li.Add(59);
-            li.Add(72);
-            li.Add(95);
-            li.Add(5);
-            li.Add(19);
-            li.Remove(59);
-            li.RemoveAt(1);
-            for (int i=0; i<li.Count; i++)
-            {
-                li[i] = i;
-            }
+            SortedList<string, int> sl = new SortedList<string, int>();
 
-            Console.WriteLine("Capacity: ");
-            Console.WriteLine(li.Capacity);
-            Console.Write("\nList: ");
-            for (int x = 0; x < li.Count; x++)
-                Console.Write(li[x] + " ");
-            li.Sort();
-            Console.Write("\nSorted: ");
-            for (int x = 0; x < li.Count; x++)
-                Console.Write(li[x] + " ");
-            //li.ForEach(+1);
+            sl.Add("Solo", 59);
+            sl.Add("A", 95);
+            sl.Add("Learn", 72);
+            //.Remove("A");
+            sl.Add("test", 99);
+            Boolean isPresent = sl.ContainsKey("A");
+            Console.WriteLine(isPresent);
+            Console.WriteLine(sl["Solo"]);
+            sl["test"] = 11;
+            Console.WriteLine("Sorted List: ");
+            foreach (string s in sl.Keys)
+                Console.WriteLine(s + ": " + sl[s]);  // Learn: 72  Solo: 59
+            Console.WriteLine("\nCount: " + sl.Count);  // 2
         }
     }
 }
